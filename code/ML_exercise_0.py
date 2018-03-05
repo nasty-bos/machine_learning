5ed09bca78599ed102f72a5d10444a7165a3bfba
Anastasiya <abosova@student.ethz.ch>
2018-03-06 00:28:13 +0100

    no changes

--- code/ML_exercise_0.py
+++ code/ML_exercise_0.py
@@ -10,16 +10,16 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import code.data as cd
 from sklearn import datasets, metrics
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
 
 # Load the datasets
-dir = "/Users/air/Documents/Master/2. Semester/intro to machine learning/code/machine_learning/data/"
 
-trainSet = pd.read_csv(os.path.join(dir, 'train.csv'), header=0, index_col=0, float_precision='round_trip')
-testSet = pd.read_csv(os.path.join(dir, 'test.csv'), header=0, index_col=0, float_precision='round_trip') 
-sampleSet = pd.read_csv(os.path.join(dir, 'sample.csv'), header=0, index_col=0, float_precision='round_trip')
+trainSet = pd.read_csv(os.path.join(cd.data_dir(), cd.DataSets.EX0.value, 'train.csv'), header=0, index_col=0, float_precision='round_trip')
+testSet = pd.read_csv(os.path.join(cd.data_dir(), cd.DataSets.EX0.value, 'test.csv'), header=0, index_col=0, float_precision='round_trip')
+sampleSet = pd.read_csv(os.path.join(cd.data_dir(), cd.DataSets.EX0.value, 'sample.csv'), header=0, index_col=0, float_precision='round_trip')
 
 linreg = LinearRegression(fit_intercept=False)
 
@@ -36,7 +36,7 @@ print(np.sqrt(metrics.mean_squared_error(testSet.loc[:, xColumns].mean(axis=1), 
 
 # write to pandas Series object 
 yPred = pd.DataFrame(y_pred, index=testSet.index, columns=['y'])
-yPred.to_csv(os.path.join(dir, 'sample.csv'))
+yPred.to_csv(os.path.join(cd.data_dir(), cd.DataSets.EX0.value, 'output .csv'))
 
 
 # check 
