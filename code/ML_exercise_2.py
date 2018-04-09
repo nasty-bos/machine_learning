af858d4c067fca0f34c1a8dbb7e34393482ee898
Anastasiya <abosova@student.ethz.ch>
2018-04-09 23:12:00 +0200

    minor changes, added cross validation

--- code/ML_exercise_2.py
+++ code/ML_exercise_2.py
@@ -17,6 +17,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import GridSearchCV
 
 # Load the datasets
 trainSet = pd.read_csv(os.path.join(cd.data_dir(), cd.DataSets.EX2.value, 'train.csv'), header=0, index_col=0, float_precision='round_trip')
@@ -27,8 +28,6 @@ sampleSet = pd.read_csv(os.path.join(cd.data_dir(), cd.DataSets.EX2.value, 'samp
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 # Fit only to the training data
-
-
 
 prcptrn = Perceptron(fit_intercept=False)
 
@@ -46,19 +45,26 @@ scaledTestSet = scaler.transform(testSet.loc[:, xColumns])
 from sklearn.neural_network import MLPClassifier
 mlpclass = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5), random_state=1, max_iter=1e3)
 
-#MLP TRAINING
+# grid-search for optimum paramters 
+paramters = {'alpha': 10.0 ** -np.arange(1, 7, 0.1)}
+optMPL = GridSearchCV(mlpclass, paramters)
+optMPL.fit(scaledTrainSet, trainSet.loc[:, yColumns].as_matrix().flatten())
+
+#  MLP TRAINING
 mlpclass.fit(scaledTrainSet, trainSet.loc[:, yColumns].as_matrix().flatten())
+optMPL.fit(scaledTrainSet, trainSet.loc[:, yColumns].as_matrix().flatten())
 
 # prediction in-sample
 y_in_sample = mlpclass.predict(scaledTrainSet)
+yOpt_in_sample = optMPL.predict(scaledTrainSet)
 
 # classification statistics
-dim = len(y_in_sample)
-inSampleConfusionMatrix = confusion_matrix(trainSet.loc[:, yColumns], y_in_sample)
+dim = len(yOpt_in_sample)
+inSampleConfusionMatrix = confusion_matrix(trainSet.loc[:, yColumns], yOpt_in_sample)
 accuracy = np.sum(np.diag(inSampleConfusionMatrix)) / dim
-print(accuracy)
+print("Using optimal parameter alpha, model accuracy %.4f " %accuracy)
 
-#prediction
+# prediction
 y_pred = mlpclass.predict(scaledTestSet)
 
 # write to pandas Series object 
